623b8019535f78d04c7567e51babc8bb34dd44b5
Melanie Molifie <mmolifie@gmail.com>
2021-07-20 09:07:43 +0200

    Update dns-server.py
    Added updated dns.py file link.

--- dns-server.py
+++ dns-server.py
@@ -2,6 +2,7 @@
     A simple server used to demonstrate the return of an IPv4 address from an APL query.
     APL address incomplete and needs the submask to be calculated. 
     Record_APL was added to dns.py at lines 39, 86, 189, 224 and 1262.
+    Link to updated DNS File: https://github.com/HackerGal/twisted-practice-repo/blob/528c51d2f613e4061d55c6fa7d3142978ed088f1/names/dns.py
     """
 from twisted.internet import task
 from twisted.names import dns
